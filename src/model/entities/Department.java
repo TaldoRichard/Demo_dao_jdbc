0cf3bb3f4325ebe78ae0aa2880e9e665146e127b
TaldoRichard <richarddegodois@gmail.com>
2024-06-14 17:17:10 -0300

    Seller entity class

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -52,7 +52,7 @@ public class Department implements Serializable{
 
 	@Override
 	public String toString() {
-		return "Department: " + id + ", name: " + name;
+		return "Department= id: " + id + ", name: " + name;
 	}
 	
 }
